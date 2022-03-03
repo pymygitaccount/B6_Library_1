c6b23d4e4b06606d1b50cec50cb5d5a88fbee6dc
pymygitaccount <apurv.p6temp@gmail.com>
2022-03-03 14:17:05 +0530

    Update settings.py

--- B6_Library/settings.py
+++ B6_Library/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'USER': 'root',
         'PASSWORD': 'root123',
         'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
-        'PORT': '3307',
+        'PORT': '3306',
     }
 }
 
